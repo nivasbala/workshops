a98fc9f511e6c89f354c9118dce4e8aa6d227812
Srini Bala <sribwork@gmail.com>
2018-05-24 14:15:32 -0700

    Completed Tomcat cookbook
    1. Added Network 192.168.34.10 to .kithcen.yml, so verfication can be done outside of vagrant box (from host)
    2. Created tomcat.service.erb using chef template create
    3. Added tomcat.service.erb
    4. Did kitchen converge - manually verified changes
       1. Destroyed and Created vagrant box
       2. Removed tomcat directory and see if its recreated
       3. Stopped tomcat service and ran kitchen converge to verify service is started
       3. Verified web and curl based 192.168.34.10:8080

--- 2_Tomcat/Tomcat/recipes/default.rb
+++ 2_Tomcat/Tomcat/recipes/default.rb
@@ -3,3 +3,95 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+#############
+# Variables #
+#############
+tomcat_binary = "apache-tomcat-8.5.20.tar.gz"
+tomcat_path = "/opt/tomcat"
+
+# Set Apt (Yum in this case) to run periodically
+apt_update 'Update the apt cache daily' do
+  frequency 86_400
+  action :periodic
+end
+
+# Run Apt (Yum Update ) now
+apt_update 'update' do
+  action:update
+end
+
+# Install Java
+package 'java-1.7.0-openjdk-devel' do
+  action:install
+end
+
+# Create Tomcat group and user
+group 'tomcat'do
+  action:create
+end
+
+user 'tomcat' do
+  group 'tomcat'
+  system true
+  home tomcat_path
+  shell '/bin/nologin'
+  action:create
+end
+
+# Delete if file already present - If you are paranoid
+#file '/tmp/apache-tomcat-8.5.20.tar.gz' do
+#  only_if {::File.exist?('/tmp/apache-tomcat-8.5.20.tar.gz') }
+#  action:delete
+#end
+    
+# Download the tomcat tarball to /tmp directory
+execute 'get-tomcat-tarball' do
+  command "wget -O /tmp/#{tomcat_binary} https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.20/bin/#{tomcat_binary}" 
+  action:run
+end
+
+# Extract the files from the tarball to /opt/tomcat
+script 'extract_tomcat' do
+  interpreter "bash"
+  cwd '/tmp'
+  code <<-EOH
+    mkdir -p #{tomcat_path }
+    tar xzf 'apache-tomcat-8.5.20.tar.gz' -C #{tomcat_path} --strip-components=1
+    EOH
+end
+
+# Set Group for tomcat directory
+execute 'tomcat-dir_set_group' do
+  command "chgrp -R tomcat #{tomcat_path}"
+  action:run
+end
+
+# Set Ownership group and user for /opt/tomcat to tomcat
+script 'set_tomcat_dir_permission' do
+  interpreter "bash"
+  cwd tomcat_path
+  code <<-EOH
+    #chmod -R 755 
+    chmod -R g+r conf/
+    chmod -R g+x conf/
+    chown -R tomcat webapps work temp logs
+    EOH
+end
+
+# Create tomcat.service file from template
+template '/etc/systemd/system/tomcat.service' do
+  source 'tomcat.service.erb'
+  action:create
+end
+
+# Restart systemd 
+execute 'systemd_restart' do
+  command 'systemctl daemon-reload'
+  action:run
+end
+
+#Start tomcat service and enable
+service 'tomcat' do
+  action [:start, :enable]
+end
